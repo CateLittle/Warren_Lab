fc7641fec7c4d3c25a2bd29acfb2b1a3b112b992
loganrower <85966936+loganrower@users.noreply.github.com>
2021-12-03 15:06:23 -0800

    Update still_image.py

--- still_image.py
+++ still_image.py
@@ -1,24 +1,29 @@
-import picamera as PiCamera
+#This program asks the user how many images they would like to take..
+# Subsquently the program is then able to preview the images for the user for 5 seconds
+# the images are saved into a folder
+from picamera import PiCamera
 from time import sleep
-from datetime import time
+from datetime import datetime
 camera = PiCamera()
-# user defined number of images
-img_num = int(input())
-# initializing global variable
-take_image = 1
-# location for saving image
-location = "/home/pi/Desktop/pythonfiles/flyimages/image%s.jpg"
 
-# formulated a while loop to take images
+# next an input was set so the user can determine how many images they would like to take
+img_num = int(input())
+
+# set global variable take_image
+take_image = 1
+
+# location for saving image
+location = "/home/pi/Desktop/pythonfiles/flyimages/%s.jpg"
+
+# next a while loop was used to go through and take images
 while take_image <= img_num:
-    # previewed the image
     camera.start_preview()
     sleep(5)
-    # stopped the preview
+    time_current = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+    # halted the preview
     camera.stop_preview()
-    # set the file name based on take_image
-    filename = location % take_image
-    # image saved to location
+    # filename was generated
+    filename = location % time_current
+    #net the image was saved...
     camera.capture(filename)
-    # continued through loop
     take_image += 1
