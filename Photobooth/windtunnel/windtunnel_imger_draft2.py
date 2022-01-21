983343abb0e5ab1b1049ba441c4933147189677e
lkrower <lkrower2000@gmail.com>
2022-01-20 16:09:00 -0800

    Added timelapse section, and quit section

--- Photobooth/windtunnel/windtunnel_imger_draft2.py
+++ Photobooth/windtunnel/windtunnel_imger_draft2.py
@@ -1,14 +1,14 @@
 # This program gives the user the option of whether or not to take a certain number of images or do a timelapse of images
-# from picamera import PiCamera
-# from time import sleep
-# from datetime import datetime
-# import os
+from picamera import PiCamera
+from time import sleep
+from datetime import datetime
+import os
 
 ## have this script be something that is user friendly... let's try to convert this to a class later on...
 ## so then this can be later utilized as a function and imported to make things easier
 
 # called PiCamera
-# camera = PiCamera()
+camera = PiCamera()
 
 # defined function for UI printing:
 def print_stats(a):
@@ -29,49 +29,58 @@ def print_stats(a):
     =========================================
     ''')
 
-# user first starts by initializing the program:
-print("Press any key to initialize the program")
-user_init = input()
-
 # next display the function:
-print_stats(user_init)
+print_stats()
 ## asked user for their option
 user_input = int(input)
 # then the user enters the while loop with another input 
 while user_input in range(1,5): # going from 1 to 4
+    # follow the below model for an if statement
     # first if statement for user_defined number of images
     if user_input == 1:
         print(" You are requesting to take X number of images would you like to proceed (y/n)")
-        img_input = input()
-        if img_input == "y":
+        img1_input = input()
+        if img1_input == "y":
             print("Please enter the number of images you would like to take")
             num_imgs = int(input())
             # now for taking the x number of images a for loop will be set up to take that many images sequentially..
             for capture in range(num_imgs):
                 # then introduce the file path and include the data and time into this as well..
-                #camera.capture('/home/pi/Desktop/img.jpg')
+                camera.capture('/home/pi/Desktop/img.jpg')
             # print_stats(img_input)
         #     # user_init = int(input())
-        # else:
-        #     print_stats(img_input)
-        #     user_init = int(input())
+        else:
+            # Determine how the user would like to proceed
+            print_stats()
+            user_input = int(input())
+#   #   #   #   #   #   #   #   #   #   #   #   
+    # second condition is if user
+    elif user_input == 2:
+        print("You are requesting to perform a timelapse for X duration, would you like to proceed (y/n)")
+        img2_input = input()
+        if img2_input == "y":
+            # set the time...
+            print("Enter the whole number of Hours")
+            time_H = int(input()) 
+            print("Enter whole number of minutes")
+            time_min = int(input())
+            print("Enter a whole number of seconds")
+            time_sec = int(input())
+            # now will perform timelapse...
+            #
+            ##
+            #
+        else:
+            # Determine how the user would like to proceed
+            print_stats()
+            user_input = int(input())
+    # this condition is if user would like to view the images...
+    elif user_input == 3:
+        # make sure the user knows that the following only works when GUI is activated on the Pi
+        print("You are requesting to look at the images taken, make sure GUI is enabled in order to do so, proceed (y/n)")
+        img3_input = input()
+        # add the rest of it...
 
-    # second elif statment will perform a timelapse 
-    # elif user_input ==2:
-    #     print("You are requesting to perform a timelapse for X duration, would you like to proceed (y/n)")
-    #     img2_input = input()
-    #     if img2_input =="y":
-    #         print("Enter the whole number of Hours")
-    #         time_H = int(input()) # 
-    #         print("Enter whole number of minutes")
-    #         time_min = int(input())
-    #         print("Enter a whole number of seconds")
-    #         time_sec = int(input())
-    #         # so now configure the timelapse
-
-#         else:
-#             print_stats(img_input)
-#             user_init = int(input())
-# if user_init == 5:
-#     quit() #this will quit the program
-#
+# last option is to quit the program
+if user_input == 5:
+    quit() #this will quit the program
