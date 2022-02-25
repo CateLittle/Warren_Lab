a9974e506a0a594ff83783971858cd2703d82ab9
lkrower <lkrower2000@gmail.com>
2022-02-25 13:35:15 -0800

    function for getting the release time

---
+++ Photobooth/windtunnel/windtunnel_time_now.py
@@ -0,0 +1,11 @@
+# Getting the Current Time Before Running Imager Script
+from datetime import datetime
+def get_time():
+    """ This function will get the current time when requested by the user"""
+    time = datetime.now()
+    time_of_release = time.strftime("%Y-%m-%d %H:%M:%S")
+    return time_of_release
+print(" does user want to take an image")
+user_response = input()
+a = get_time()
+print(a)
