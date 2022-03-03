9e525bd7c0ba7e348a3c50a46b89a0a544e404fd
lkrower <lkrower2000@gmail.com>
2022-03-03 11:52:15 -0800

    new edits

--- Photobooth/windtunnel/windtunnel_time_now.py
+++ Photobooth/windtunnel/windtunnel_time_now.py
@@ -6,6 +6,6 @@ def get_time():
     time_of_release = time.strftime("%Y-%m-%d %H:%M:%S")
     return time_of_release
 print(" does user want to take an image")
-user_response = input()
+#user_response = input()
 a = get_time()
 print(a)
